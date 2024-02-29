8a87481c76de2ea40ad7948e787b7b546ce7f851
Havilash Sivaratnam <havilash2005@gmail.com>
2024-02-29 13:50:44 +0100

    test: add missing tests

--- backend/careminder/patient/tests.py
+++ backend/careminder/patient/tests.py
@@ -1,67 +1,66 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from rest_framework.test import APIRequestFactory, force_authenticate
 from patient.views import PatientListCreateView, PatientRetrieveUpdateDestroyView
 from patient.models import Patient
+from staff.models import Staff
 from unittest.mock import patch
 
 
+@override_settings(
+    REST_FRAMEWORK={
+        "DEFAULT_AUTHENTICATION_CLASSES": [],
+        "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+    }
+)
 class TestPatient(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.view = PatientListCreateView.as_view()
+        self.retrieve_view = PatientRetrieveUpdateDestroyView.as_view()
         self.uri = "/patient/"
 
         self.patient = Patient.objects.create(
-            first_name="Test", last_name="Patient", age=30
+            first_name="Test",
+            last_name="Patient",
+            age=30,
+            severity=Patient.Severity.LOW,
+            medical_progress=Patient.MedicalProgress.WAIT,
         )
 
-    @patch("patient.models.Patient.objects.create")
-    def test_list_patients(self, mock_create):
-        mock_create.return_value = self.patient
+    @patch("patient.models.Patient.objects.all")
+    def test_list_patients(self, mock_all):
+        mock_all.return_value = [self.patient]
         request = self.factory.get(self.uri)
+        request.testing = True
         response = self.view(request)
         self.assertEqual(response.status_code, 200)
-
-    @patch("patient.models.Patient.objects.create")
-    def test_create_patient(self, mock_create):
-        mock_create.return_value = self.patient
-        data = {
-            "first_name": "New",
-            "last_name": "Patient",
-            "age": 40,
-            "doctor_first_visit": False,
-            "hospitalization": Patient.Severity.LOW,
-        }
-        request = self.factory.post(self.uri, data=data)
-        response = self.view(request)
-        self.assertEqual(response.status_code, 201)
 
     @patch("patient.models.Patient.objects.get")
     def test_retrieve_patient(self, mock_get):
         mock_get.return_value = self.patient
-        view = PatientRetrieveUpdateDestroyView.as_view()
-        uri = f"/patient/{self.patient.id}/"  # type: ignore
+        uri = f"/patient/{self.patient.id}/"
         request = self.factory.get(uri)
-        response = view(request, pk=self.patient.id)  # type: ignore
+        request.testing = True
+        response = self.retrieve_view(request, pk=self.patient.id)
         self.assertEqual(response.status_code, 200)
 
     @patch("patient.models.Patient.objects.get")
     def test_update_patient(self, mock_get):
         mock_get.return_value = self.patient
-        view = PatientRetrieveUpdateDestroyView.as_view()
-        uri = f"/patient/{self.patient.id}/"  # type: ignore
+        uri = f"/patient/{self.patient.id}/"
         data = {
             "first_name": "Updated",
         }
         request = self.factory.put(uri, data=data)
-        response = view(request, pk=self.patient.id)  # type: ignore
+        request.testing = True
+        response = self.retrieve_view(request, pk=self.patient.id)
         self.assertEqual(response.status_code, 200)
 
     @patch("patient.models.Patient.objects.get")
     def test_delete_patient(self, mock_get):
         mock_get.return_value = self.patient
-        view = PatientRetrieveUpdateDestroyView.as_view()
-        uri = f"/patient/{self.patient.id}/"  # type: ignore
+        uri = f"/patient/{self.patient.id}/"
         request = self.factory.delete(uri)
-        response = view(request, pk=self.patient.id)  # type: ignore
+        request.testing = True
+        response = self.retrieve_view(request, pk=self.patient.id)
         self.assertEqual(response.status_code, 204)
--- backend/careminder/tablet/tests.py
+++ backend/careminder/tablet/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from tablet.views import (
+    TabletListCreateView,
+    TabletRetrieveUpdateDestroyView,
+)
+from tablet.models import Tablet
+from patient.models import Patient
+from settings.models import Area
+from unittest.mock import patch
 
-# Create your tests here.
+
+@override_settings(
+    REST_FRAMEWORK={
+        "DEFAULT_AUTHENTICATION_CLASSES": [],
+        "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+    }
+)
+class TestTablet(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.list_create_view = TabletListCreateView.as_view()
+        self.retrieve_update_destroy_view = TabletRetrieveUpdateDestroyView.as_view()
+        self.uri = "/tablet/"
+
+        self.area = Area.objects.create(name="Test Area")
+        self.patient = Patient.objects.create(first_name="Test", last_name="Patient")
+
+        self.tablet = Tablet.objects.create(name="Test Tablet", area=self.area)
+
+    @patch("tablet.models.Tablet.objects.all")
+    def test_list_tablets(self, mock_all):
+        mock_all.return_value = [self.tablet]
+        request = self.factory.get(self.uri)
+        request.testing = True
+        response = self.list_create_view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @patch("tablet.models.Tablet.objects.get")
+    def test_retrieve_tablet(self, mock_get):
+        mock_get.return_value = self.tablet
+        uri = f"/tablet/{self.tablet.id}/"
+        request = self.factory.get(uri)
+        request.testing = True
+        response = self.retrieve_update_destroy_view(request, pk=self.tablet.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @patch("tablet.models.Tablet.objects.get")
+    def test_update_tablet(self, mock_get):
+        mock_get.return_value = self.tablet
+        uri = f"/tablet/{self.tablet.id}/"
+        data = {
+            "name": "Updated Tablet",
+        }
+        request = self.factory.put(uri, data=data)
+        request.testing = True
+        response = self.retrieve_update_destroy_view(request, pk=self.tablet.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @patch("tablet.models.Tablet.objects.get")
+    def test_delete_tablet(self, mock_get):
+        mock_get.return_value = self.tablet
+        uri = f"/tablet/{self.tablet.id}/"
+        request = self.factory.delete(uri)
+        request.testing = True
+        response = self.retrieve_update_destroy_view(request, pk=self.tablet.id)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
