e6e0ff0d08562bb5082b34ba5f39f09704cca325
Havilash Sivaratnam <havilash2005@gmail.com>
2024-03-21 12:43:40 +0100

    chore: update

--- frontend/web/src/pages/App/App.jsx
+++ frontend/web/src/pages/App/App.jsx
@@ -32,7 +32,7 @@ function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<Navigate to="/nurse/login" />} />
+				<Route path="/*" element={<Navigate to="/nurse/login" />} />
 
 				<Route path="/nurse/*" element={<Navigate to="/nurse/home" />} />
 				<Route path="/nurse" element={<Navigate to="/nurse/login" />} />
