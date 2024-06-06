629a291e4643bdf8f0b3a7a65de27503ca7ec298
aro135603 <aro135603@stud.gibb.ch>
2024-06-06 11:10:58 +0200

    feat: ASL4ALL

---
+++ frontend/web/src/pages/ASL4ALL/ASL4ALL.jsx
@@ -0,0 +1,110 @@
+import React, { useState, useEffect, useRef } from "react";
+import { GestureRecognizer, FilesetResolver } from "@mediapipe/tasks-vision";
+import model from "../../models/gesture_recognizer.task";
+ 
+const MainComponent = () => {
+	const [loading, setLoading] = useState(true);
+	const [gestureRecognizer, setGestureRecognizer] = useState(null);
+	const [currentLetter, setCurrentLetter] = useState("");
+	const [currentSentence, setCurrentSentence] = useState("");
+	const videoRef = useRef(null);
+	const runningModeRef = useRef("VIDEO");
+	let count = 0;
+	let prev = "";
+ 
+	async function predictWebcam(video, lastVideoTime) {
+		let nowInMs = Date.now();
+		let results;
+		if (video.currentTime !== lastVideoTime) {
+			lastVideoTime = video.currentTime;
+			results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+		}
+ 
+		try {
+			setCurrentLetter(results.gestures[0][0].categoryName);
+ 
+			if (results.gestures[0][0].categoryName === prev) {
+				count += 1;
+			} else {
+				prev = results.gestures[0][0].categoryName;
+				count = 0;
+			}
+			if (count === 2) {
+				count = 0;
+				setCurrentSentence(
+					(prevSentence) => prevSentence + results.gestures[0][0].categoryName
+				);
+				prev = "";
+			}
+		} catch (error) {
+			setCurrentLetter("");
+		}
+	}
+ 
+	useEffect(() => {
+		const createGestureRecognizer = async () => {
+			const vision = await FilesetResolver.forVisionTasks(
+				"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+			);
+			const recognizer = await GestureRecognizer.createFromOptions(vision, {
+				baseOptions: {
+					modelAssetPath: model,
+					delegate: "CPU",
+				},
+				runningMode: runningModeRef.current, // Use current value
+			});
+			setGestureRecognizer(recognizer);
+			setLoading(false);
+		};
+		createGestureRecognizer();
+	}, []);
+ 
+	useEffect(() => {
+		if (loading || !gestureRecognizer) {
+			return;
+		} else {
+			const video = videoRef.current;
+ 
+			let lastVideoTime = -1;
+ 
+			const constraints = { video: true };
+			navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+				video.srcObject = stream;
+				video.addEventListener("loadeddata", () => {
+					if (gestureRecognizer) {
+						predictWebcam(video, lastVideoTime);
+						let intervalId = setInterval(
+							() => predictWebcam(video, lastVideoTime),
+							250
+						);
+						return () => clearInterval(intervalId);
+					}
+				});
+			});
+		}
+	}, [loading, gestureRecognizer]);
+ 
+	return (
+		<>
+			<div>
+				<video
+					id="webcam"
+					ref={videoRef}
+					autoPlay
+					className="h-full w-full object-cover object-center"
+				/>
+ 
+				<div>
+					<div className="box-content h-32 w-32 absolute top-0 right-0 flex justify-center items-center bg-gray-600 rounded-2xl">
+						{" "}
+						{currentLetter}{" "}
+					</div>
+					<p className="current_sentence">{currentSentence} </p>
+				</div>
+			</div>
+				
+		</>
+	);
+};
+ 
+export default MainComponent;
--- frontend/web/src/pages/Patient/Home/Home.jsx
+++ frontend/web/src/pages/Patient/Home/Home.jsx
@@ -29,6 +29,10 @@ export default function Home({ session }) {
 
 	const patient = data.patient;
 
+	const navigateToASL4ALL = () => {
+		navigate("/patient/ASL4ALL");
+	};
+
 	const fetchSettings = async () => {
 		if (!session.user) return;
 		try {
@@ -52,6 +56,10 @@ export default function Home({ session }) {
 						<h1>{settings.hospital_title}</h1>
 						<h2>{settings.hospital_description} </h2>
 					</div>
+
+					<button className="bg-red-600 button-left" onClick={navigateToASL4ALL}>
+        Switch to ASL
+					</button>
 
 					<div className="rq-container">
 						<div className="question-container">
